efe9a0d06f61518dfb252ded9c4e567b13bc72ca
farahmzn <arahlan18@gmail.com>
2026-05-06 10:50:28 +0800

    Update Dashboard.tsx

--- src/app/pages/Dashboard.tsx
+++ src/app/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
       <h2></h2>
 
       <iframe
-        src="https://70.153.136.4:8080/dashboard/1a478330-d9ce-11f0-8463-1fcaa679e0db?publicId=78366650-46fc-11f1-927d-5dc2aadba241"
+        src="http://70.153.136.4:8080/dashboard/1a478330-d9ce-11f0-8463-1fcaa679e0db?publicId=78366650-46fc-11f1-927d-5dc2aadba241"
         width="100%"
         height="900"
         style={{ border: "none" }}
