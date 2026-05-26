3ef718688999da616b17d1027c27941d95f59473
Farah <f.mazlan@live.iium.edu.my>
2026-05-26 11:31:50 +0800

    fix: add .history to gitignore, remove from tracking

--- .history/src/main_20260521003401.tsx
+++
@@ -1,9 +0,0 @@
-
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-
-  <button onClick={testSend}>Test Email</button>
-  
--- .history/src/main_20260521003938.tsx
+++
@@ -1,8 +0,0 @@
-
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App";
-  import "./styles/index.css";
- import { useEffect } from "react";
-import { sendFloodEmail } from "./app/utils/email";
-
-  createRoot(document.getElementById("root")!).render(<App />);
